5fa677df54ed30724ad832c3a9f37242544aae10
Araqsya Bardaxchyan <Araqsya.Bardaxchyan@mail.ru>
2020-07-06 17:31:13 +0300

    d

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
   });
 });
 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+
+var img = new Image(100, 200);
+img.src = '../img/5172.750x0.jpg';
+console.log(img);
